112476a85ca6f44754ef87f641dca67308b734ef
David Silva <contatodavidmaia@gmail.com>
2023-10-04 18:41:29 -0300

    feat: remove env files

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,9 @@
 import { app } from './app'
-import { env } from './env'
 
 app
   .listen({
     host: '0.0.0.0',
-    port: env.PORT,
+    port: 3333,
   })
   .then(() => {
     console.log('🚀 HTTP Server is running!')
